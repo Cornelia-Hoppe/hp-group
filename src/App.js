b1a2d4fe8e5a72a106696bcff8427f3d9434cb89
Yrla Hackzell <yrla.hackzell@hotmail.com>
2022-03-28 15:13:40 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./style.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./views/Home"
 import Gryffindor from "./views/Gryffindor"
