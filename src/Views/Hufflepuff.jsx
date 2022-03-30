98187f1a68e1db6c28d3846d044f0209bd895327
Yrla Hackzell <yrla.hackzell@hotmail.com>
2022-03-30 11:31:06 +0200

    deletions and styles

--- src/Views/Hufflepuff.jsx
+++ src/Views/Hufflepuff.jsx
@@ -1,13 +1,13 @@
 import "../styles/Hufflepuff.css"
 import React from 'react';
 import Main from '../Components/Hufflepuff/Main';
-import Nav from '../Components/Hufflepuff/Nav';
 import { ApiProvider } from '../Components/Hufflepuff/ApiProvider';
+import Nav from '../Components/Nav'
 function Hufflepuff() {
   return (
 <ApiProvider>
     <div className="Hufflepuff">
-      <Nav />
+    <Nav />
       <Main />
     </div>   
     </ApiProvider>
